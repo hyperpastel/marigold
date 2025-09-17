ae9ad4f277274245caf66454f869f8cd6403e411
hyperpastel <vmutze04@gmail.com>
2025-09-18 00:28:31 +0200

    feat(nix): add build support

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "";
+  description = "A discord bot that downloads youtube videos as mp3s";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -16,31 +16,71 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
 
         buildPackages = with pkgs; [
           dpp
-          yt-dlp-light
-          ffmpeg
-          # will also need openssl and zlib for runtime
+          openssl
+          zlib
         ];
+
+        cmakeBuild = pkgs.stdenv.mkDerivation {
+          name = "marigold";
+          version = "1.0.0";
+
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.makeWrapper
+            pkgs.pkg-config
+          ];
+
+          buildInputs = buildPackages ++ [
+            pkgs.ffmpeg
+            pkgs.yt-dlp-light
+          ];
+
+          # We don't have tests (yet?)
+          doCheck = false;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp marigold $out/bin
+          '';
+
+          postFixup = ''
+            find
+             wrapProgram $out/bin/marigold --set PATH "${
+               pkgs.lib.makeBinPath [
+                 pkgs.yt-dlp-light
+                 pkgs.ffmpeg
+               ]
+             }"
+          '';
+
+        };
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
+            ffmpeg
+            yt-dlp-light
             clang-tools
-            clang
             cmake
-            openssl.dev
-            zlib.dev
           ]
           ++ buildPackages;
 
           shellHook = ''
             export PROJECT_PREFIX=marigold
-            echo Entered dev shell for project marigold
+            echo "Entered dev shell for project marigold"
           '';
+        };
+
+        packages = {
+          default = cmakeBuild;
+          marigold = cmakeBuild;
         };
 
       }
